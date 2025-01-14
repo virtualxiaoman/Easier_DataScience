72c5d49d903d0570e63a1372fb5f9cf7d97903db
virtualxiaoman <2958694743@qq.com>
2025-01-14 22:00:02 +0800

    更新D_HP_3

---
+++ data_analyse/ML_HUB/Demo_House_Prices/D_HP_other.py
@@ -0,0 +1,86 @@
+# 1. 垃圾回收
+print("---------- 1. 垃圾回收 -------------")
+import gc
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+# 启用调试模式，保存所有不可达对象
+gc.set_debug(gc.DEBUG_SAVEALL)
+
+# 创建循环引用
+node1 = Node(1)
+node2 = Node(2)
+node1.next = node2
+node2.next = node1
+print(f"node1.next.value: {node1.next.value}")
+print(f"node2.next.value: {node2.next.value}")
+
+# 删除引用，但对象仍存在（循环引用）
+del node1
+del node2
+
+# 手动触发垃圾回收
+gc.collect()
+
+# 打印不可达对象
+for obj in gc.garbage:
+    print(f"未释放的对象: {obj}")
+
+for obj in gc.garbage:
+    del obj
+gc.garbage.clear()
+print(f"gc.garbage 长度: {len(gc.garbage)}")  # 应输出 0
+
+
+# 2. CountVectorizer
+print("---------- 2. CountVectorizer -------------")
+from sklearn.feature_extraction.text import CountVectorizer
+
+# 样本文本数据
+documents = [
+    "I love programming.",
+    "I love machine learning.",
+    "Machine learning is fun."
+]
+
+# 创建 CountVectorizer 对象
+vectorizer = CountVectorizer()
+
+# 将文本数据转化为特征矩阵
+X = vectorizer.fit_transform(documents)
+
+# 查看词汇表
+print("Vocabulary:", vectorizer.get_feature_names_out())
+
+# 查看文档-词矩阵
+print("Document-Term Matrix:")
+print(X.toarray())
+
+import pandas as pd
+
+# 创建一个包含标签列的 DataFrame
+data = {
+    'ID': [1, 2, 3],
+    'tags': ['python, pandas, data', 'machine learning, deep learning', 'data analysis, python']
+}
+
+df = pd.DataFrame(data)
+
+# 打印原始 DataFrame
+print("原始 DataFrame:")
+print(df)
+
+# 使用 str.split() 将 'tags' 列按逗号分割成列表
+df['tags_split'] = df['tags'].str.split(', ')
+print("\n使用 str.split() 处理后的 DataFrame:")
+print(df)
+
+# 使用 str.get_dummies() 创建每个标签的独热编码
+df_tags_dummies = df['tags_split'].str.join('|').str.get_dummies()
+print("\n使用 str.get_dummies() 处理后的 DataFrame:")
+print(df_tags_dummies)
